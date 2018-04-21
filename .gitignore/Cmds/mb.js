8a753ed1271983e25039ec42a44f6f64e04682ab
L.V <35047671+LuffyVanquish@users.noreply.github.com>
2018-04-21 21:08:38 +0200

    Update mb.js

--- .gitignore/Cmds/mb.js
+++ .gitignore/Cmds/mb.js
@@ -1,21 +1,41 @@
 const commands = require ("./commands")
 const Discord = require("discord.js");
+const config = require("../config")
 
 module.exports = class mb extends commands {
 
 
     static match (message){
-        return message.content.startWith(prefix + "mb")
+        return message.content.startWith(config.prefix + "help")
     }
 
     static action (message) {
 
         var meme_sembed = new Discord.RichEmbed()
             .setColor("#FEFE00")
-            .addField(":space_invader:  **__MemeBox__** :space_invader: ","`*blop` \n`*salt` \n`*scuse` \n`*knife` \n`*pot` \n`*blbl` \n`*phot` \n`*pol` \n`*view` \n`*men` \n`*bear` \n")
+
+            .addField(":space_invader:  **__MemeBox__** :space_invader: ",
+                "\n`" + config.prefix + "blop`" +
+                "\n`" + config.prefix + "salt`" +
+                "\n`" + config.prefix + "scuse`" +
+                "\n`" + config.prefix + "knife`" +
+                "\n`" + config.prefix + "pot`" +
+                "\n`" + config.prefix + "blbl`" +
+                "\n`" + config.prefix + "phot`" +
+                "\n`" + config.prefix + "pol`" +
+                "\n`" + config.prefix + "view`" +
+                "\n`" + config.prefix + "men`" +
+                "\n`" + config.prefix + "bear`" +
+                "\n"
+            )
+
             .setFooter("[WIP]")
-            message.delete();
-            message.channel.sendEmbed(meme_sembed);
-            console.log("MemeBox demandé")
+
+        message.delete();
+        message.author.createDM().then(channel => {
+            channel.send(meme_sembed);
+        });
+        console.log("MemeBox demandé")
+
     }
 }
